ab29c0f7b488252a92cb0e4b893eaa82e498af56
soruh <mail@soruh.de>
2026-06-03 20:23:03 +0200

    Migrate renderer from minifb to winit and pixels

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,30 @@ mod random;
 mod simulation;
 
 use chrono::Local;
-use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use pixels::{Pixels, SurfaceTexture};
 use std::{
     cmp::Ordering,
     collections::VecDeque,
     env, mem,
+    num::NonZero,
     path::Path,
     process::ExitCode,
     sync::{
+        Arc,
         atomic::{AtomicBool, Ordering as AtomicOrdering},
         mpsc,
     },
     thread,
     time::{Duration, Instant},
+};
+use toml::ser;
+use winit::{
+    application::ApplicationHandler,
+    dpi::PhysicalSize,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowAttributes, WindowId},
 };
 
 use crate::{
@@ -305,59 +316,295 @@ impl<'sim> Simulator<'sim> {
     }
 }
 
+/// Simulation App state.
+/// Contains all state needed to render a frame.
+struct App<'app, 'frame, const PALETTE_RES: usize> {
+    /// The config watcher, used for live config changes.
+    config_watcher: ConfigWatcher,
+
+    /// The Frame which represents the render buffer.
+    frame: Option<Frame<'frame>>,
+
+    /// The Window we are rendering to.
+    window: Option<Arc<Window>>,
+
+    /// The Window attributes of our window.
+    window_attributes: WindowAttributes,
+
+    /// The color palette.
+    palette: Palette<PALETTE_RES>,
+
+    /// Flag to signal the simulation thread to terminate.
+    is_running: &'app AtomicBool,
+
+    /// Channel to send the current renderer grid back to the sim thread.
+    render_to_sim_tx: mpsc::Sender<Grid>,
+
+    /// Channel to receive the new simulation grid state.
+    sim_to_render_rx: mpsc::Receiver<Grid>,
+
+    /// Channel to send configuration updates to the simulation thread.
+    config_tx: mpsc::Sender<Config>,
+
+    /// Time at which the next frame is due to be rendered.
+    frame_timeout: Instant,
+
+    /// Intermediate grid used for rendering. Wrapped in an Option so we can take ownership
+    /// temporarily while swapping it with the simulation thread.
+    render_grid: Option<Grid>,
+}
+
+impl<'app, const PALETTE_RES: usize> App<'app, '_, PALETTE_RES> {
+    /// Initialize the App.
+    fn new(
+        is_running: &'app AtomicBool,
+        render_to_sim_tx: mpsc::Sender<Grid>,
+        sim_to_render_rx: mpsc::Receiver<Grid>,
+        config_tx: mpsc::Sender<Config>,
+        config_watcher: ConfigWatcher,
+        palette: Palette<PALETTE_RES>,
+        render_grid: Grid,
+    ) -> Self {
+        let window_attributes = WindowAttributes::default()
+            .with_title("Trippy Ants (Space: save screenshot, Esc: quit)")
+            .with_inner_size(PhysicalSize::new(WIDTH, HEIGHT))
+            .with_resizable(false);
+
+        Self {
+            config_watcher,
+            frame: None,
+            window: None,
+            window_attributes,
+            palette,
+            is_running,
+            render_to_sim_tx,
+            sim_to_render_rx,
+            config_tx,
+            frame_timeout: Instant::now() + FRAME_TIME,
+            render_grid: Some(render_grid),
+        }
+    }
+
+    /// Update the config if the `config_watcher` found an updated config.
+    ///
+    /// # Panics
+    /// If the simulation thread has exited.
+    fn update_config(&mut self) {
+        if let Some(new_config) = self.config_watcher.watch_for_update() {
+            println!("config updated");
+            if let Ok(config_str) = ser::to_string(&new_config) {
+                println!("loaded config:\n{config_str}");
+            }
+
+            self.palette = Palette::<PALETTE_RES>::new(&new_config.colors);
+
+            // Forward the updated config ownership to the simulation thread
+            self.config_tx
+                .send(new_config)
+                .expect("Simulation thread refused to accept the config");
+        }
+    }
+
+    /// Helper to cleanly exit.
+    fn exit(&self, event_loop: &ActiveEventLoop) {
+        self.is_running.store(false, AtomicOrdering::Relaxed);
+        event_loop.exit();
+    }
+}
+
+#[expect(
+    clippy::missing_trait_methods,
+    reason = "We want to ignore all other events"
+)]
+impl<const PALETTE_RES: usize> ApplicationHandler for App<'_, '_, PALETTE_RES> {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let win = Arc::from(
+            event_loop
+                .create_window(self.window_attributes.clone())
+                .expect("Failed to create window"),
+        );
+        let window_size = win.inner_size();
+
+        eprintln!(
+            "Initializing {}x{} Window",
+            window_size.width, window_size.height
+        );
+
+        let surface_texture =
+            SurfaceTexture::new(window_size.width, window_size.height, Arc::clone(&win));
+        let pixels = Pixels::new(u32::from(WIDTH), u32::from(HEIGHT), surface_texture)
+            .expect("Failed to initialize Pixels buffer");
+
+        self.frame = Some(Frame::new(WIDTH, HEIGHT, pixels));
+        self.window = Some(win);
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        if Instant::now() >= self.frame_timeout
+            && let Some(window) = self.window.as_ref()
+        {
+            window.request_redraw();
+        }
+        // This instructs the winit event loop to sleep the thread until the exact timeout.
+        event_loop.set_control_flow(ControlFlow::WaitUntil(self.frame_timeout));
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        _window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        #[expect(
+            clippy::pattern_type_mismatch,
+            reason = "We want a mutable refernce to each field"
+        )]
+        let Self {
+            frame,
+            window,
+            palette,
+            render_to_sim_tx,
+            sim_to_render_rx,
+            frame_timeout,
+            render_grid,
+            ..
+        } = self;
+
+        let Some(frame) = frame.as_mut() else {
+            return;
+        };
+
+        let Some(_window) = window.as_ref() else {
+            return;
+        };
+
+        #[expect(
+            clippy::wildcard_enum_match_arm,
+            reason = "We want to ignore all other events"
+        )]
+        match event {
+            WindowEvent::CloseRequested => self.exit(event_loop),
+            WindowEvent::KeyboardInput {
+                event: input_event, ..
+            } if input_event.state.is_pressed() => match input_event.physical_key {
+                PhysicalKey::Code(KeyCode::Escape) => self.exit(event_loop),
+                PhysicalKey::Code(KeyCode::Space) => {
+                    let filename = format!(
+                        "trippy-ants_{}.png",
+                        Local::now().format("%Y-%m-%d_%H-%M-%S")
+                    );
+                    match frame.save_png(Path::new(&filename)) {
+                        Ok(()) => println!("saved {filename}"),
+                        Err(error) => eprintln!("failed to save {filename}: {error}"),
+                    }
+                }
+                _ => {}
+            },
+            WindowEvent::RedrawRequested => {
+                // Request a frame update by sending our buffer to the simulation thread.
+                if let Some(grid) = render_grid.take()
+                    && render_to_sim_tx.send(grid).is_ok()
+                    && let Ok(new_grid) = sim_to_render_rx.recv()
+                {
+                    *render_grid = Some(new_grid);
+                }
+
+                if let Some(grid) = render_grid.as_ref() {
+                    frame.update(grid, palette);
+                } else {
+                    // We lost the grid because the channel disconnected (sim thread died).
+                    self.exit(event_loop);
+                    return;
+                }
+
+                if let Err(err) = frame.render() {
+                    eprintln!("pixels.render() failed: {err}");
+                    self.exit(event_loop);
+                    return;
+                }
+
+                // Advance the timeout. A loop prevents queueing instant back-to-back renders
+                // if the application lagged momentarily.
+                let now = Instant::now();
+                while *frame_timeout <= now {
+                    *frame_timeout += FRAME_TIME;
+                }
+
+                self.update_config();
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() -> ExitCode {
+    let num_sim_threads = env::var("RAYON_NUM_THREADS")
+        .ok()
+        .and_then(|x| x.parse::<usize>().ok())
+        .unwrap_or_else(|| {
+            thread::available_parallelism()
+                .map_or(1, NonZero::get)
+                .min(8)
+        });
+
     let mut config_watcher = ConfigWatcher::new();
+
     let config = if let Some(config_file) = env::args().nth(1) {
         match config_watcher.load_config(config_file) {
-            Ok(config) => config,
-            Err(error) => {
-                eprintln!("{error}");
+            Ok(cfg) => cfg,
+            Err(err) => {
+                eprintln!("{err}");
                 return ExitCode::FAILURE;
             }
         }
     } else {
+        println!("no config file provided, using default config");
         DEFAULT_CONFIG
     };
 
-    let mut palette = Palette::<1024>::new(&config.colors);
-    let mut window = Window::new(
-        "Trippy Ants (Space: save screenshot, Esc: quit)",
-        usize::from(WIDTH),
-        usize::from(HEIGHT),
-        WindowOptions {
-            resize: false,
-            scale: minifb::Scale::X1,
-            ..WindowOptions::default()
-        },
-    )
-    .expect("window");
-    window.set_target_fps(0);
+    if let Ok(config_str) = ser::to_string(&config) {
+        println!("loaded config:\n{config_str}");
+    }
 
+    let palette = Palette::<1024>::new(&config.colors);
     let simulation = Simulation::new(WIDTH, HEIGHT, &config.world);
     let render_grid = simulation.make_scratch_grid();
-
     let agents = (0..config.agent.count)
         .map(|index| Agent::new(&config, WIDTH, HEIGHT, index))
         .collect::<Vec<_>>();
 
     let is_running = AtomicBool::new(true);
+
     let (render_to_sim_tx, render_to_sim_rx) = mpsc::channel::<Grid>();
     let (sim_to_render_tx, sim_to_render_rx) = mpsc::channel::<Grid>();
     let (config_tx, config_rx) = mpsc::channel::<Config>();
 
-    let mut frame = Frame::new(WIDTH, HEIGHT);
-    let mut render_grid_opt = Some(render_grid);
-    let mut frame_timeout = Instant::now() + FRAME_TIME;
+    let Ok(event_loop) = EventLoop::new() else {
+        eprintln!("Failed to initialize window event loop");
+        return ExitCode::FAILURE;
+    };
 
     thread::scope(|scope| {
-        let is_running_ref = &is_running;
-        let _sim_thread = thread::Builder::new()
+        #[expect(
+            clippy::shadow_same,
+            reason = "Explicitly capturing reference for thread scope"
+        )]
+        let is_running = &is_running;
+
+        let simulation_thread = thread::Builder::new()
             .name("sim_worker_0".to_owned())
             .spawn_scoped(scope, move || {
+                rayon::ThreadPoolBuilder::new()
+                    .thread_name(|i| format!("sim_worker_{i}"))
+                    .num_threads(num_sim_threads)
+                    .use_current_thread()
+                    .build_global()
+                    .expect("Failed to build rayon threadpool");
+
                 Simulator::new(
                     simulation,
                     agents,
-                    is_running_ref,
+                    is_running,
                     render_to_sim_rx,
                     sim_to_render_tx,
                     config_rx,
@@ -365,47 +612,26 @@ fn main() -> ExitCode {
                 .run();
             });
 
-        while window.is_open() && !window.is_key_pressed(Key::Escape, KeyRepeat::No) {
-            if Instant::now() >= frame_timeout {
-                if let Some(grid) = render_grid_opt.take() {
-                    render_to_sim_tx
-                        .send(grid)
-                        .expect("Unexpected channel closure");
-                }
-                if let Ok(new_grid) = sim_to_render_rx.try_recv() {
-                    render_grid_opt = Some(new_grid);
-                }
+        let mut app = App::<1024>::new(
+            is_running,
+            render_to_sim_tx,
+            sim_to_render_rx,
+            config_tx,
+            config_watcher,
+            palette,
+            render_grid,
+        );
 
-                if let Some(grid) = render_grid_opt.as_ref() {
-                    frame.update(grid, &palette);
-                    frame.update_window(&mut window);
-                }
+        let app_error = event_loop.run_app(&mut app).is_err();
 
-                if window.is_key_pressed(Key::Space, KeyRepeat::No) {
-                    let filename = format!(
-                        "trippy-ants_{}.png",
-                        Local::now().format("%Y-%m-%d_%H-%M-%S")
-                    );
-                    frame
-                        .save_png(Path::new(&filename))
-                        .expect("Failed to save png");
-                }
-
-                let now = Instant::now();
-                while frame_timeout <= now {
-                    frame_timeout += FRAME_TIME;
-                }
-
-                if let Some(new_config) = config_watcher.watch_for_update() {
-                    palette = Palette::<1024>::new(&new_config.colors);
-                    config_tx
-                        .send(new_config)
-                        .expect("Unexpected channel closure");
-                }
-            }
-            thread::sleep(Duration::from_millis(1));
-        }
         is_running.store(false, AtomicOrdering::Relaxed);
-    });
-    ExitCode::SUCCESS
+
+        let simulation_error = simulation_thread.map_or(true, |handle| handle.join().is_err());
+
+        if app_error || simulation_error {
+            ExitCode::FAILURE
+        } else {
+            ExitCode::SUCCESS
+        }
+    })
 }
